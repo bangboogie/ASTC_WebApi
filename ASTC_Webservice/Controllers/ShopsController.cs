44ee61a8db0cebe699b6cc3416623d762f4045bf
Christian Bo Petersen <1061823@ucn.dk>
2018-11-26 09:21:52 +0100

    Cors FIX 2

--- ASTC_Webservice/Controllers/ShopsController.cs
+++ ASTC_Webservice/Controllers/ShopsController.cs
@@ -10,6 +10,7 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using ASTC_Webservice.DAL;
 using ASTC_Webservice.Models;
+using System.Web.Http.Cors;
 
 namespace ASTC_Webservice.Controllers
 {
